5011b29f4b2990e5a40ecfd0bde4dc8d3afc1d00
Andrew.Mille5a9 <Andrew.Mille5a9@outlook.com>
2021-01-25 00:05:59 -0500

    deleted resume3.1

--- resume-3.1/resume-3.1/ClientApp/src/components/DecoLine.js
+++
@@ -1,9 +0,0 @@
-﻿import React, { Component } from 'react';
-import { Col } from 'reactstrap';
-
-export class DecoLine extends Component {
-
-    render() {
-        return <Col lg="auto" className="align body-right"><div className="decorationline" /></Col>;
-    }
-}
